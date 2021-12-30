b17d02e3edea0e7c33b753e1550b345f97cf20af
cameronteconway <cameronteconway@outlook.com>
2021-12-30 11:22:49 +0000

    intersection observer update

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -9,6 +9,7 @@ const useOnScreen = options => {
 
     useEffect(() => {
         const observer = new IntersectionObserver(([entry]) => {
+            // const isAbove = entry.boundingClientRect.y < entry.rootBounds.y;
             setVisible(entry.isIntersecting);
         }, options);
 
@@ -23,44 +24,52 @@ const useOnScreen = options => {
 };
 
 const SocialMedia = () => {
-    const [setRef, visible] = useOnScreen({ threshold: 1 });
+    const [setRef, visible] = useOnScreen({
+        rootMargin: '60px',
+        threshold: 0.8,
+    });
 
     return (
-        <section ref={setRef}>
-            <div
-                className={`wrapper animatedFadeInUp ${
-                    visible ? 'animated fadeInUp' : ''
-                }`}
-            >
-                <div className='social-media'>
-                    <h2>Explore #CTEC on Social Media</h2>
-                    <ul>
-                        <li>
-                            <a href='#test' title='Visit our LinkedIn page'>
-                                <i className='bi bi-linkedin'></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href='#test' title='Visit our Twitter page'>
-                                <i className='bi bi-twitter'></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href='#test' title='Visit our YouTube page'>
-                                <i className='bi bi-youtube'></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href='#test' title='Visit our Instagram page'>
-                                <i className='bi bi-instagram'></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href='#test' title='Visit our Facebook page'>
-                                <i className='bi bi-facebook'></i>
-                            </a>
-                        </li>
-                    </ul>
+        <section>
+            <div ref={setRef}>
+                <div
+                    className={`wrapper animatedFadeInUp ${
+                        visible ? 'animated fadeInUp' : ''
+                    }`}
+                >
+                    <div className='social-media'>
+                        <h2>Explore #CTEC on Social Media</h2>
+                        <ul>
+                            <li>
+                                <a href='#test' title='Visit our LinkedIn page'>
+                                    <i className='bi bi-linkedin'></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href='#test' title='Visit our Twitter page'>
+                                    <i className='bi bi-twitter'></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href='#test' title='Visit our YouTube page'>
+                                    <i className='bi bi-youtube'></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href='#test'
+                                    title='Visit our Instagram page'
+                                >
+                                    <i className='bi bi-instagram'></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href='#test' title='Visit our Facebook page'>
+                                    <i className='bi bi-facebook'></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </section>
