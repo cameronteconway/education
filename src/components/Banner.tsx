cee3ae289886b235360aa229639357dff84ac593
cameronteconway <cameronteconway@outlook.com>
2023-07-17 17:31:12 +0100

    work

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -8,7 +8,7 @@ const Banner = () => {
                 title='Campus building in summer surrounded by trees'
             />
             <div className='banner__info'>
-                <span className='banner__title'>Open Days</span>
+                <span className='banner__title'>Open Day s</span>
                 <p className='banner__text'>Sign up to book your open day.</p>
                 <a className='banner__link' href='/#'>
                     Book open day<span className='banner__right-arrow'></span>
